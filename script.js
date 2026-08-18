717675d900e692ae8255cbbe71bd9dab1ba00a9a
buju14133-max <buju14133@gmail.com>
2026-08-18 20:09:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,10 @@
   ==========================================
 */
 
-const WHATSAPP_LINK = "http://wa.me/+2348180112699";
+const WHATSAPP_LINK = "http://wa.me/2348180112699";
 
 const whatsappButton = document.getElementById("whatsappButton");
 
-if (WHATSAPP_LINK && WHATSAPP_LINK !== "http://wa.me/+2348180112699") {
-  whatsappButton.href = http://wa.me/+2348180112699;
+if (WHATSAPP_LINK && WHATSAPP_LINK !== "http://wa.me/2348180112699") {
+  whatsappButton.href = http://wa.me/2348180112699;
 }
